4aef16ccd4c9b1b7fe11d523f93a4803b85b22ef
Urvesh <urveshpatel94@yahoo.com>
2015-09-01 16:57:17 -0400

    added image uploading

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var multer = require('multer')
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var controllers = require('./controllers/controller');
 
 var app = express();
 
@@ -21,9 +23,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'uploads')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.get('/uploads', controllers.index);
+
+app.post('/uploads/images/', multer({ storage: controllers.storage}).array('images'), controllers.postUpload);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -56,5 +62,8 @@ app.use(function(err, req, res, next) {
     });
 });
 
+app.listen(3000, function() {
+    console.log('Listening on port: 3000');
+});
 
 module.exports = app;
